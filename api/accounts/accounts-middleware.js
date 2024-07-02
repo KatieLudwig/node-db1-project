1bb06dc8b81425ceff7b4839f9a4234a1a5e1a32
Katie Ludwig <katierludwig@gmail.com>
2024-07-01 20:14:15 -0700

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,56 @@
+const db = require('../../data/db-config');
+const Account = require('./accounts-model');
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  const { name, budget } = req.body;
+
+  if (name === undefined || budget === undefined) {
+    res.status(400).json({ message: 'name and budget are required' });
+  } 
+
+  if (typeof name != 'string' 
+    || name.trim().length < 3 
+    || name.trim().length > 100) {
+      res.status(400).json({ message: 'name of account must be between 3 and 100 characters' });
+    }
+
+  if (isNaN(budget)) {
+    return res.status(400).json({ message: 'budget of account must be a number' });
+  }
+
+  if (budget < 0 || budget > 1000000) {
+    return res.status(400).json({ message: 'budget of account is too large or too small' });
+  }
+
+  next();
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  const { name } = req.body;
+
+  try {
+    const existingAccount = await db('accounts')
+    .where('name', name.trim()).first();
+    if (existingAccount) {
+      return res.status(400).json({ message: 'that name is taken' });
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const account = await Account.getById(id);
+    if (!account) {
+      return res.status(404).json({ message: 'account not found' });
+    }
+    req.account = account;
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
